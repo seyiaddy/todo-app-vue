d1ab7a0124029dd4a420c0dfd671f495abc0694b
Seyi <oluwaseyiadisaa@gmail.com>
2021-01-04 15:19:11 +0100

    hardcoded list

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,43 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <h1>Todo App</h1>
+    <v-container>
+      <v-row align="center" justify="space-between">
+        <v-col md="4">
+          <v-text-field label="Enter Todo" @keypress.enter="clickHandler" />
+        </v-col>
+        <v-col md="8">
+          <v-btn depressed elevation="1" @click="clickHandler">Add Todo</v-btn>
+        </v-col>
+      </v-row>
+      <Todo />
+    </v-container>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Todo from "./components/Todo";
 
 export default {
-  name: 'App',
-
+  name: "App",
   components: {
-    HelloWorld,
+    Todo
   },
+  data() {
+    return {
 
-  data: () => ({
-    //
-  }),
+    }
+  },
+  methods: {
+    clickHandler() {
+      alert("hey")
+    }
+  }
 };
 </script>
+
+<style scoped>
+* {
+  text-align: center;
+}
+</style>
